37394388676d1b267bb6e10873ae61cef99524cc
Bradley <b.pickering@hotmail.com>
2023-03-26 15:54:20 +0100

    Update ##PackageFileAsEXE.ps1

--- ##PackageFileAsEXE.ps1
+++ ##PackageFileAsEXE.ps1
@@ -1,7 +1,8 @@
 ##PackageFileAsEXE
 
-Import-Module ps2exe
 
-Invoke-ps2exe "ps1 File Location" "Exe File Location" -noConsole -noOutput
+#First file location should be location of PS script, second location is the target location for exe. Includes a file icon - must be a .ico
+Import-Module ps2exe
+Invoke-ps2exe "D:\Coding\Powershell\audiotoggle.ps1" "D:\Coding\Powershell\audiotoggle.exe" -iconfile "D:\Coding\Powershell\Assets\speaker.ico" -noConsole -noOutput
 
 ##exe file can then easily be used for macros
