0b9b1bf7afc34205d0f9fe408aed6357d1665dc1
Bradley <b.pickering@hotmail.com>
2023-03-26 15:51:25 +0100

    Update ##AudioDeviceSwap.ps1

--- ##AudioDeviceSwap.ps1
+++ ##AudioDeviceSwap.ps1
@@ -1,16 +1,22 @@
 ##AudioDeviceSwap
 
 ##Import modules
-
 import-module AudioDeviceCmdlets
 import-module BurntToast
 
+##Assign your variables - change variables to suit and ensure name matches your devices
+$Soundbar = get-audiodevice -list | where name -like "*Soundbar*"
+$Headset = get-audiodevice -list | where name -like "*Gaming*"
+$Current = Get-AudioDevice -Playback
 
-##Run the script
+##Assign the locations of your media
+$Sound = new-object System.Media.SoundPlayer
+$Sound.SoundLocation = "D:\Coding\Powershell\Assets\Windows XP Startup.wav"
+$Image = "D:\Coding\Powershell\Assets\speaker.png"
 
-##Set Where-Object name -like to the start of the device name set-audiodevice -id to the id of the device to swap to
-if (get-audiodevice -list | Where-Object type -like "Playback" | Where-Object name -like "Speak*" | Where-Object default -eq "True"){set-audiodevice -id ""}
-elseif (get-audiodevice -list | Where-Object type -like "Playback" | Where-Object name -like "Sound*" | Where-Object default -eq "True"){set-audiodevice -id ""}
+##Script
+if ($Current.ID -eq $Headset.ID){set-audiodevice -ID $Soundbar.ID; New-BurntToastNotification -Text "Soundbar Activated" -AppLogo $Image}
+elseif ($Current.ID -eq $Soundbar.ID){set-audiodevice -ID $Headset.ID; New-BurntToastNotification -Text "Headset Activated" -AppLogo $Image}
 
-if (get-audiodevice -list | Where-Object type -like "Playback" | Where-Object name -like "Speak*" | Where-Object default -eq "True"){New-BurntToastNotification -Text "Headset Activated" -AppLogo "C:\Users\Public"}
-elseif (get-audiodevice -list | Where-Object type -like "Playback" | Where-Object name -like "Sound*" | Where-Object default -eq "True"){New-BurntToastNotification -Text "Soundbar Activated" -AppLogo "C:\Users\Public"}
+Start-Sleep -Seconds 1
+$Sound.Play()
